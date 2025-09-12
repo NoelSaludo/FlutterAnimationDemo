fb8adcc38828bd4a671c6ec9c4fa51c5beb29eef
Noel Saludo <noelsaludo17@gmail.com>
2025-09-12 19:06:31 +0800

    removed static keyword and implemented goBack

--- lib/shared/PageNavigator.dart
+++ lib/shared/PageNavigator.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class PageNavigator {
-  static void goTo(BuildContext context, Widget page) {
+  void goTo(BuildContext context, Widget page) {
     Navigator.push(context, MaterialPageRoute<void>(builder: (context) => page));
+  }
+
+  void goBack(BuildContext context) {
+    Navigator.pop(context);
   }
 }
