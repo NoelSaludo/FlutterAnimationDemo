3b221a1adfdbb3b0a81b30c1e314a7de1946cf94
Noel Saludo <noelsaludo17@gmail.com>
2025-09-12 19:07:40 +0800

    Implemented LottiePage class and removed unnecessary features

--- lib/Widget/Home.dart
+++ lib/Widget/Home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutteranimationdemo/shared/ButtonBuilder.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -11,11 +13,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  ButtonBuilder buttonBuilder = ButtonBuilder();
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +27,23 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            buttonBuilder.BuildButtonToLottie(context),
           ],
         ),
       ),
+    );
+  }
+}
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+class LottiePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: ButtonBuilder().BuildBackElivatedButton(context)),
     );
   }
 }
