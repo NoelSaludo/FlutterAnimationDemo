ab016ca09ec77ab322ad0224c018bed0a90558f4
Noel Saludo <noelsaludo17@gmail.com>
2025-09-12 19:03:12 +0800

    Implemented BuildLottieButton and BuildBackButton

--- lib/shared/ButtonBuilder.dart
+++ lib/shared/ButtonBuilder.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutteranimationdemo/Widget/Home.dart';
 import 'package:flutteranimationdemo/shared/PageNavigator.dart';
 
 class ButtonBuilder {
-  Widget buildRouteTextButton(BuildContext context, Widget page, String child) {
+  Widget BuildLottieButton(BuildContext context) {
     return TextButton(
-      child: Text(child),
-      onPressed: () => PageNavigator.goTo(context, page),
+      child: Text("Lottie Page"),
+      onPressed: () => PageNavigator().goTo(context, LottiePage()),
+    );
+  }
+
+  Widget BuildBackElivated(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => PageNavigator().goBack(context),
+      child: Text("Back"),
     );
   }
 }
