05c3c6b65cbd7e82320bf4f138a52f4a87c368ab
Noel Saludo <noelsaludo17@gmail.com>
2025-09-16 22:57:18 +0800

    Feat: Add LoadingPage and integrate it into navigation flow

---
+++ lib/Pages/LoadingPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutteranimationdemo/Pages/Home.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key, required this.PageToLoad});
+
+  final Widget PageToLoad;
+  Future<String> loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    return "Data Loaded";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: loadData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text("Error: ${snapshot.error}")),
+          );
+        } else {
+          return MyHomePage(title: "Home Page"); 
+        }
+      },
+    );
+  }
+}
