f4f8f71880123952e8becf57a939852fb4ff2dc1
Noel Saludo <noelsaludo17@gmail.com>
2025-09-16 21:26:38 +0800

    Refactored file structure and added LoginPage.dart

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutteranimationdemo/Widget/LottiePage.dart';
-import 'package:flutteranimationdemo/Widget/RadialAnimationPage.dart';
 import 'package:flutteranimationdemo/shared/ButtonBuilder.dart';
+import 'package:flutteranimationdemo/shared/PageNavigator.dart';
+import 'package:flutteranimationdemo/Pages/LoginPage.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  final bool isLoggedIn = false;
+
+  static const String testUsername = "testuser";
+  static const String testPassword = "password123";
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -14,6 +18,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   ButtonBuilder buttonBuilder = ButtonBuilder();
+  LoginPage loginPage = LoginPage();
+
+  @override
+  void initState() {
+    if (!widget.isLoggedIn) {
+      PageNavigator().goTo(context,loginPage);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,4 +45,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
