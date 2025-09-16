28c4b426817e924f7c2542aaa7133e575ce2e0bc
Noel Saludo <noelsaludo17@gmail.com>
2025-09-16 22:57:18 +0800

    Feat: Add LoadingPage and integrate it into navigation flow

--- lib/utils/PageNavigator.dart
+++ lib/utils/PageNavigator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutteranimationdemo/Pages/Home.dart';
+import 'package:flutteranimationdemo/Pages/LoadingPage.dart';
 
 class PageNavigator {
   void goTo(BuildContext context, Widget page) {
@@ -15,9 +16,13 @@ class PageNavigator {
 
   void goToHome(BuildContext context) {
     Navigator.pop(context);
+    MyHomePage mhp = MyHomePage(title: "Home Page");
+    LoadingPage lp = LoadingPage(PageToLoad: mhp);
+
+    // Loading page will navigate to Home page after loading
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => MyHomePage(title: "MyLibrary")),
+      MaterialPageRoute(builder: (context) => lp),
     );
   }
 }
