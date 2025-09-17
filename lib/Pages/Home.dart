a1c2b48e27802d1c7bb0b5848acf58a0b0555849
Noel Saludo <noelsaludo17@gmail.com>
2025-09-17 14:00:50 +0800

    Feat: Update Home page layout with GridView and item cards

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutteranimationdemo/utils/ButtonBuilder.dart';
-import 'package:flutteranimationdemo/Pages/LoginPage.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -17,17 +16,34 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: Text(widget.title)),
+      body: GridView.count(crossAxisCount: 2, children: _fillView(context)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: const Icon(Icons.person),
       ),
-      body: Center(
-        child: buttonBuilder.BuildTextButtonToPage(
-          context,
-          LoginPage(),
-          Text("Go to Login Page"),
-        ),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
     );
   }
 
+  _fillView(BuildContext context) {
+    List<Widget> items = [];
+    for (int i = 0; i < 20; i++) {
+      var item = Text("Item $i", style: TextStyle(fontSize: 24));
+      items.add(_createItemCard(item));
+    }
+    return items;
+  }
+
+  Widget _createItemCard(Text item) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
+      child: Container(
+        alignment: Alignment.center,
+        height: 500,
+        width: 200,
+        child: item,
+      ),
+    );
+  }
 }
