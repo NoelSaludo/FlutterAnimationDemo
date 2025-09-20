6cfdbd19ff03cdd3444e3c3244cc3c9cb8405704
joelaguzar <aguzarjoel07@gmail.com>
2025-09-21 02:59:51 +0800

    feat: enhance ItemDetailPage UI

--- lib/Widget/ItemDetailSliverAppBar.dart
+++ lib/Widget/ItemDetailSliverAppBar.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutteranimationdemo/models/Book.dart';
 
-class ItemDetailSliverAppBar extends StatelessWidget {
+class ItemDetailSliverAppBar extends StatefulWidget {
   final Book book;
   final int itemIndex;
+  final bool isFavorite;
   final VoidCallback? onBack;
   final VoidCallback? onFavorite;
 
@@ -11,27 +12,79 @@ class ItemDetailSliverAppBar extends StatelessWidget {
     Key? key,
     required this.book,
     required this.itemIndex,
+    this.isFavorite = false,
     this.onBack,
     this.onFavorite,
   }) : super(key: key);
 
   @override
+  State<ItemDetailSliverAppBar> createState() => _ItemDetailSliverAppBarState();
+}
+
+class _ItemDetailSliverAppBarState extends State<ItemDetailSliverAppBar>
+    with TickerProviderStateMixin {
+  late AnimationController _heroController;
+  late AnimationController _buttonController;
+  late Animation<double> _heroAnimation;
+  late Animation<double> _buttonAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _heroController = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    _buttonController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _heroAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _heroController,
+      curve: Curves.easeOutCubic,
+    ));
+
+    _buttonAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _buttonController,
+      curve: Curves.elasticOut,
+    ));
+
+    _heroController.forward();
+    _buttonController.forward();
+  }
+
+  @override
+  void dispose() {
+    _heroController.dispose();
+    _buttonController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      expandedHeight: 300,
+      expandedHeight: 400,
       pinned: true,
       backgroundColor: Colors.transparent,
+      elevation: 0,
       flexibleSpace: FlexibleSpaceBar(
         background: _buildHeroBackground(),
       ),
       leading: _buildLeading(context),
-      actions: [_buildFavoriteAction(context)],
+      actions: [_buildFavoriteAction(context), _buildShareAction(context)],
     );
   }
 
   Widget _buildHeroBackground() {
     return Hero(
-      tag: 'item_image_$itemIndex',
+      tag: 'item_image_${widget.itemIndex}',
       child: _buildBackgroundContainer(),
     );
   }
@@ -39,11 +92,91 @@ class ItemDetailSliverAppBar extends StatelessWidget {
   Widget _buildBackgroundContainer() {
     return Container(
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(book.coverImageUrl),
-          fit: BoxFit.cover,
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 20,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+        child: Stack(
+          children: [
+            _buildImageBackground(),
+            _buildGradientOverlay(),
+            _buildBookInfo(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildImageBackground() {
+    return Positioned.fill(
+      child: Image.network(
+        widget.book.coverImageUrl,
+        fit: BoxFit.cover,
+        errorBuilder: (context, error, stackTrace) {
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF667eea),
+                  Color(0xFF764ba2),
+                ],
+              ),
+            ),
+            child: const Center(
+              child: Icon(
+                Icons.image_not_supported,
+                color: Colors.white,
+                size: 60,
+              ),
+            ),
+          );
+        },
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF667eea),
+                  Color(0xFF764ba2),
+                ],
+              ),
+            ),
+            child: Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                    loadingProgress.expectedTotalBytes!
+                    : null,
+                strokeWidth: 3,
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildGradientOverlay() {
+    return Positioned.fill(
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -51,46 +184,219 @@ class ItemDetailSliverAppBar extends StatelessWidget {
             end: Alignment.bottomCenter,
             colors: [
               Colors.transparent,
-              Colors.black.withOpacity(0.7),
+              Colors.black.withOpacity(0.3),
+              Colors.black.withOpacity(0.8),
             ],
+            stops: const [0.0, 0.6, 1.0],
           ),
         ),
       ),
     );
   }
 
-  Widget _buildLeading(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.black54,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.white),
-        onPressed: onBack ?? () => Navigator.pop(context),
+  Widget _buildBookInfo() {
+    return Positioned(
+      bottom: 20,
+      left: 20,
+      right: 20,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.book.title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              shadows: [
+                Shadow(
+                  color: Colors.black54,
+                  blurRadius: 10,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'by ${widget.book.author}',
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.9),
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              shadows: const [
+                Shadow(
+                  color: Colors.black54,
+                  blurRadius: 8,
+                  offset: Offset(0, 1),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildFavoriteAction(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.black54,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: IconButton(
-        icon: const Icon(Icons.favorite_border, color: Colors.white),
-        onPressed: onFavorite ?? () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Added to favorites!'),
-              duration: Duration(seconds: 2),
+  Widget _buildLeading(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _buttonAnimation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _buttonAnimation.value,
+          child: Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 4,
+              left: 8,
+              right: 8,
+              bottom: 8,
             ),
-          );
-        },
-      ),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.3),
+                width: 1,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(25),
+                onTap: widget.onBack ?? () => Navigator.pop(context),
+                child: const Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildFavoriteAction(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _buttonAnimation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _buttonAnimation.value,
+          child: Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 4,
+              left: 8,
+              right: 8,
+              bottom: 8,
+            ),
+            decoration: BoxDecoration(
+              color: widget.isFavorite
+                  ? Colors.red.withOpacity(0.9)
+                  : Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.3),
+                width: 1,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(25),
+                onTap: widget.onFavorite,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Icon(
+                    widget.isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildShareAction(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _buttonAnimation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _buttonAnimation.value,
+          child: Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 4,
+              left: 8,
+              right: 16,
+              bottom: 8,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.3),
+                width: 1,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(25),
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: const Text('Share functionality!'),
+                      duration: const Duration(seconds: 2),
+                      behavior: SnackBarBehavior.floating,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  );
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.share_rounded,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
